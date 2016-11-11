acd3fa9302a82c8b8077f327453fef86b678a98a
mtorgovitsky <m.torgovitsky@gmail.com>
2016-11-11 08:49:52 +0200

    Add project files.

---
+++ Recursion01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Recursion01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumRecurs(369));
+        }
+
+        #region Recursive Sum
+        //Recursive function that calculates the sum of the digits.
+        static int SumRecurs(int number)
+        {
+            if (number != 0)
+            {
+                return (number % 10 + SumRecurs(number / 10));
+            }
+            else return 0;
+        }
+        #endregion
+    }
+}
